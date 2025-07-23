785459e5f1892902006090d6ef3d20eaeccb53fe
artlu99 <alu99.sm@gmail.com>
2025-07-23 04:17:45 +0200

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
+import path from "node:path";
 import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import path from "node:path";
 import { defineConfig } from "vite";
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-	plugins: [react(), cloudflare(), tailwindcss()],
+	plugins: [
+		react(), cloudflare(), tailwindcss(),
+		VitePWA({ registerType: 'autoUpdate' })
+	],
 	resolve: {
 		alias: {
 			"~": path.resolve(__dirname, "./src/frontend"),
